145a03eeba15fee17e133709cabd52c7f2a859bc
arjav2002 <arjavgarg@gmail.com>
2020-10-23 17:40:20 +0530

    Enabled view binding

--- app/src/main/java/com/example/trackandtrigger/MainActivity.java
+++ app/src/main/java/com/example/trackandtrigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.trackandtrigger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent registerIntent = new Intent(getBaseContext(), RegisterActivity.class);
+        this.startActivity(registerIntent);
     }
 }
