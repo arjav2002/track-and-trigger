9d2bdeef8af57ff7a708f3acb27018f3bc5e807f
arjav2002 <arjavgarg@gmail.com>
2020-10-24 00:01:59 +0530

    Google sign in fixed, package named fixed fully (hopefully)

--- app/src/main/java/com/oopcows/trackandtrigger/RegisterActivity.java
+++ app/src/main/java/com/oopcows/trackandtrigger/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.oopcows.trackandtrigger;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.oopcows.trackandtrigger.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -25,9 +28,7 @@ public class RegisterActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         binding.googleSignInButton.setOnClickListener(googleSignInListener());
     }
@@ -36,6 +37,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         GoogleSignInAccount gAccount = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(gAccount);
     }
 
     private View.OnClickListener googleSignInListener() {
@@ -46,5 +48,39 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivityForResult(signInIntent, G_SIGN_IN);
             }
         };
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == G_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleGoogleSignInResult(task);
+        }
+    }
+
+    private void handleGoogleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        GoogleSignInAccount gAccount = null;
+        try {
+            gAccount = completedTask.getResult(ApiException.class);
+        } catch (ApiException e) {
+            Log.w("sign-in error", "signInResult:failed code=" + e.getStatusCode());
+        } finally {
+            updateUI(gAccount);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount gAccount) {
+        if(gAccount == null) {
+
+        }
+        else {
+            Intent dashboardIntent = new Intent(getBaseContext(), DashboardActivity.class);
+            dashboardIntent.putExtra("Google", gAccount);
+            startActivity(dashboardIntent);
+            finish();
+            System.out.println("lolxd");
+        }
     }
 }
