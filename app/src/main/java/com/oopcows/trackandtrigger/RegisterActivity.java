5498607ea6763290850650b5619f40e512f5f3a3
arjav2002 <arjavgarg@gmail.com>
2020-10-23 23:05:05 +0530

    Added functionality of google sign in and other fixes

--- app/src/main/java/com/oopcows/trackandtrigger/RegisterActivity.java
+++ app/src/main/java/com/oopcows/trackandtrigger/RegisterActivity.java
@@ -1,17 +1,50 @@
 package com.oopcows.trackandtrigger;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.oopcows.trackandtrigger.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    ActivityRegisterBinding binding;
+    private static final int G_SIGN_IN = 1;
+    private ActivityRegisterBinding binding;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        binding.googleSignInButton.setOnClickListener(googleSignInListener());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount gAccount = GoogleSignIn.getLastSignedInAccount(this);
+    }
+
+    private View.OnClickListener googleSignInListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, G_SIGN_IN);
+            }
+        };
     }
 }
