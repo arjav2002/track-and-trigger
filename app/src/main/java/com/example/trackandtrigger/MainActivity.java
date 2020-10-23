a65b6fe91e4276c065d60c305c9085707334400d
arjav2002 <arjavgarg@gmail.com>
2020-10-23 17:42:17 +0530

    #2 Pressing back button on RegisterActivity: App exits now on pressing back

--- app/src/main/java/com/example/trackandtrigger/MainActivity.java
+++ app/src/main/java/com/example/trackandtrigger/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
         Intent registerIntent = new Intent(getBaseContext(), RegisterActivity.class);
         this.startActivity(registerIntent);
+        finish();
     }
 }
