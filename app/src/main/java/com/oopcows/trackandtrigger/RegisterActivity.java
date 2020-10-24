d32d2cbeee9622349ed7b6221cf5063578bbcd33
arjav2002 <arjavgarg@gmail.com>
2020-10-24 08:30:23 +0530

    modified RegisterActivity to only ask for username and password

--- app/src/main/java/com/oopcows/trackandtrigger/RegisterActivity.java
+++ app/src/main/java/com/oopcows/trackandtrigger/RegisterActivity.java
@@ -20,7 +20,6 @@ public class RegisterActivity extends AppCompatActivity {
 
     private static final int G_SIGN_IN = 1;
     private ActivityRegisterBinding binding;
-    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +28,8 @@ public class RegisterActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        binding.googleSignInButton.setOnClickListener(googleSignInListener());
+        binding.signInButton.setOnClickListener(signInListener());
         binding.registerButton.setOnClickListener(registerButtonListener());
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        GoogleSignInAccount gAccount = GoogleSignIn.getLastSignedInAccount(this);
-        if(gAccount != null) {
-            updateUI(gAccount);
-        }
     }
 
     private View.OnClickListener registerButtonListener() {
@@ -50,70 +38,24 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username = String.valueOf(binding.usernameField.getText());
                 String password = String.valueOf(binding.passwordField.getText());
-                String gmail = String.valueOf(binding.gmailField.getText());
-                String phno = String.valueOf(binding.phnoField.getText());
+
                 // validate inputs
-                goToNext(new UserAccount(username, password, gmail, phno), OtpActivity.class);
             }
         };
     }
 
-    private View.OnClickListener googleSignInListener() {
+    private View.OnClickListener signInListener() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-                startActivityForResult(signInIntent, G_SIGN_IN);
+                // do firebase/server bullshit and fill up userAccount @vraj/@arjav
+                String username = String.valueOf(binding.usernameField.getText());
+                String password = String.valueOf(binding.passwordField.getText());
+                UserAccount userAccount = null; // fill this with that
+                Intent dashboardIntent = new Intent(getBaseContext(), DashboardActivity.class);
+                dashboardIntent.putExtra("com.oopcows.trackandtrigger.helpers.UserAccount", userAccount);
+                startActivity(dashboardIntent);
             }
         };
-    }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == G_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleGoogleSignInResult(task);
-        }
-    }
-
-    private void handleGoogleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        GoogleSignInAccount gAccount = null;
-        try {
-            gAccount = completedTask.getResult(ApiException.class);
-        } catch (ApiException e) {
-            Log.w("sign-in error", "signInResult:failed code=" + e.getStatusCode());
-        } finally {
-            updateUI(gAccount);
-        }
-    }
-
-    private void updateUI(GoogleSignInAccount gAccount) {
-        if(gAccount == null) {
-            // show in some label that google sign in didn't work
-        }
-        else {
-            // check with firebase if account is a new one
-            boolean accountIsNew = true;
-            UserAccount userAccount = null;
-            Intent nextActivityIntent = null;
-            if(accountIsNew) {
-                goToNext(new UserAccount("", "", gAccount.getEmail(), ""), OtpActivity.class);
-            }
-            else {
-                // fill up username, password, phno, email using firebase
-                String username, password, gmailId, phno;
-                username = password = gmailId = phno = null;
-                goToNext(new UserAccount(username, password, gmailId, phno), DashboardActivity.class);
-            }
-        }
-    }
-
-    private void goToNext(UserAccount userAccount, Class goToActivity) {
-        Intent nextActivityIntent = new Intent(getBaseContext(), goToActivity);
-        nextActivityIntent.putExtra("com.oopcows.trackandtrigger.helpers.UserAccount", userAccount);
-        startActivity(nextActivityIntent);
-        finish();
-    }
 }
