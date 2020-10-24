7e57cbdc7ef930afcb7706b4756f69f3f48dbd4a
arjav2002 <arjavgarg@gmail.com>
2020-10-24 07:10:11 +0530

    Added otp activity

--- app/src/main/java/com/oopcows/trackandtrigger/OtpActivity.java
+++ app/src/main/java/com/oopcows/trackandtrigger/OtpActivity.java
@@ -3,19 +3,32 @@ package com.oopcows.trackandtrigger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.oopcows.trackandtrigger.databinding.ActivityOtpBinding;
 import com.oopcows.trackandtrigger.helpers.UserAccount;
 
 public class OtpActivity extends AppCompatActivity {
 
+    //@vraj make this work
+
     // allows the last minute changing of details and sending otp to verify
     private UserAccount userAccount;
+    private ActivityOtpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otp);
+        binding = ActivityOtpBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         userAccount = getIntent().getExtras().getParcelable("com.oopcows.trackandtrigger.helpers.UserAccount");
+        binding.usernameField.setText(userAccount.getUsername());
+        binding.passwordField.setText(userAccount.getPassword());
+        binding.gmailField.setText(userAccount.getGmailId());
+        binding.phnoField.setText(userAccount.getPhno());
+
+        // send otp @vraj
     }
 }
