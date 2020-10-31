2d8a6fd92b9e0c1d233e23d499a42640ecb2228b
Victor-Titan <vrajgandhi@hotmail.com>
2020-10-31 14:34:17 +0530

    Linked app to firebase, saved user data on Firebase

--- app/src/main/java/com/oopcows/trackandtrigger/regandlogin/RegisterActivity.java
+++ app/src/main/java/com/oopcows/trackandtrigger/regandlogin/RegisterActivity.java
@@ -6,11 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.oopcows.trackandtrigger.dashboard.DashboardActivity;
 import com.oopcows.trackandtrigger.databinding.ActivityRegisterBinding;
 import com.oopcows.trackandtrigger.helpers.Profession;
 import com.oopcows.trackandtrigger.helpers.UserAccount;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.oopcows.trackandtrigger.helpers.CowConstants.USERS_TABLE_NAME;
 import static com.oopcows.trackandtrigger.helpers.CowConstants.USER_ACCOUNT_INTENT_KEY;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -53,6 +60,22 @@ public class RegisterActivity extends AppCompatActivity {
 
     // @vraj fill this up
     private void uploadAccountToFirebase(UserAccount userAccount) {
-
+        Map<String, Object> v = new HashMap<>();
+        v.put("name", userAccount.getUsername());
+        v.put("gmailId", userAccount.getGmailId());
+        v.put("phoneNumber", userAccount.getPhno());
+        v.put("profession", userAccount.getProfession());
+        v.put("password", password);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        db.collection(USERS_TABLE_NAME)
+                .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .set(v)
+                .addOnSuccessListener((OnSuccessListener) (aVoid) -> {
+                    System.out.println("Success");
+                })
+                .addOnFailureListener((e) -> {
+                    System.out.println("Fail");
+                });
     }
 }
