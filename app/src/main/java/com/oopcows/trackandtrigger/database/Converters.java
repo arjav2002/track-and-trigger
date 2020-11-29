025a112aaf38e5cf2d2c3e4385555c79f2c482c1
SubsTheTechnomancer <54163143+SubsTheTechnomancer@users.noreply.github.com>
2020-11-29 16:37:18 +0530

    Revert "Merge branch 'main' into XML_Materials"
    This reverts commit f9a953e14249be14a9bef87271b364f2a29d087c, reversing
    changes made to c7027d8127525bcc700d0848bf7b4552a5de9010.

--- app/src/main/java/com/oopcows/trackandtrigger/database/Converters.java
+++ app/src/main/java/com/oopcows/trackandtrigger/database/Converters.java
@@ -28,7 +28,7 @@ public class Converters {
     public static String fromTodos(ArrayList<Todo> todos) {
         StringBuilder result= new StringBuilder();
         for(Todo todo : todos) {
-            result.append(todo.getTask()).append(todo).append('\1').append(todo.isDone() ? '\1' : '\2').append(todo.getTimeString()).append('\3');
+            result.append(todo.getTask()).append(todo).append('\1').append(todo.isDone() ? '\1' : '\2');
         }
         return result.toString();
     }
@@ -37,30 +37,18 @@ public class Converters {
     public static ArrayList<Todo> fromDelimitedString(String delimitedString) {
         ArrayList<Todo> todos = new ArrayList<Todo>();
         StringBuilder task = new StringBuilder();
-        boolean isDone = false;
-        StringBuilder dateTime = new StringBuilder();
-        boolean readingDatetime = false;
         for(int i = 0; i < delimitedString.length(); i++) {
             char ch = delimitedString.charAt(i);
             if(ch == '\1') {
-                readingDatetime = true;
+                todos.add(new Todo(task.toString(), true));
+                task = new StringBuilder();
             }
             else if(ch == '\2') {
-                isDone = true;
-                readingDatetime = true;
-            }
-            else if(ch == '\3') {
-                todos.add(new Todo(task.toString(), isDone, dateTime.toString()));
+                todos.add(new Todo(task.toString(), false));
                 task = new StringBuilder();
-                readingDatetime = false;
             }
             else {
-                if(readingDatetime) {
-                    dateTime.append(ch);
-                }
-                else {
-                    task.append(ch);
-                }
+                task.append(ch);
             }
         }
         return todos;
@@ -70,7 +58,7 @@ public class Converters {
     public static String getData(ArrayList<CategoryItem> items) {
         StringBuilder data = new StringBuilder();
         for(CategoryItem item : items) {
-            data.append(item.getItemName()).append('\1').append(item.getImgPath()).append('\1').append(item.getQuantity()).append('\1').append(item.getDownPath()).append('\1');
+            data.append(item.getItemName()).append('\1').append(item.getImgPath()).append('\1').append(item.getQuantity()).append('\1');
         }
         return data.toString();
     }
@@ -82,8 +70,6 @@ public class Converters {
         StringBuilder itemName = new StringBuilder();
         StringBuilder imgPath = new StringBuilder();
         StringBuilder quantity = new StringBuilder();
-        StringBuilder downPath = new StringBuilder();
-      
         for(int i = 0; i < data.length(); i++) {
             char ch = data.charAt(i);
             if(ch == '\1') delimiters++;
@@ -97,10 +83,7 @@ public class Converters {
                 quantity.append(ch);
             }
             else if(delimiters == 3) {
-                downPath.append(ch);
-            }
-            else if(delimiters==4) {
-                items.add(new CategoryItem(itemName.toString(), imgPath.toString(), Integer.parseInt(quantity.toString()), downPath.toString()));
+                items.add(new CategoryItem(itemName.toString(), imgPath.toString(), Integer.parseInt(quantity.toString())));
                 itemName = new StringBuilder();
                 imgPath = new StringBuilder();
                 quantity = new StringBuilder();
